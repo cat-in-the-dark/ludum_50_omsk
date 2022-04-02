de3f6586ff1c938922d00b0a4ab4f01736f0b4a1
Ilya Siganov <ilya.blan4@gmail.com>
2022-04-03 02:01:32 +0600

    fix fonts

--- Assets/Scripts/SleepEffect.cs
+++ Assets/Scripts/SleepEffect.cs
@@ -1,4 +1,3 @@
-using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 
@@ -9,8 +8,9 @@ public class SleepEffect : MonoBehaviour
     private float timer;
     private GameObject camObj;
     private Vector3 camRotatorInitPos;
-    
+
     private Vignette vignette;
+    private DepthOfField depthOfField;
     private State state;
     
     private void Start()
@@ -18,6 +18,7 @@ public class SleepEffect : MonoBehaviour
         timer = 0f;
         state = State.Find();
         vignette = ppVolume.profile.GetSetting<Vignette>();
+        depthOfField = ppVolume.profile.GetSetting<DepthOfField>();
 
         camObj = Camera.main.gameObject;
         camRotatorInitPos = camObj.transform.position;
@@ -27,18 +28,38 @@ public class SleepEffect : MonoBehaviour
     {
         if (state.energyLevel < 0)
         {
-            vignette.intensity.value = -state.energyLevel * 1.5f;
+            vignette.intensity.value = Mathf.Lerp(
+                vignette.intensity.value,
+                -state.energyLevel * 1.5f,
+                Time.time * 0.01f
+            );
+            
+            
+            depthOfField.focalLength.value = Mathf.Lerp(
+                depthOfField.focalLength.value,
+                -state.energyLevel * 60f,
+                Time.time * 0.01f
+            );
+            
+            
             Rotatat();
+        }
+        else
+        {
+            vignette.intensity.value = 0;
         }
     }
 
     private void Rotatat()
     {
         timer += Time.deltaTime;
+        
+        var multiplier = 1 - Mathf.InverseLerp(state.minEnergyLevel, 0, state.energyLevel);
+        
         var pos = camObj.transform.position;
-        pos.y = camRotatorInitPos.y + Mathf.Sin(timer * 1.1f) * 0.05f;
-        pos.x = camRotatorInitPos.x + Mathf.Cos(timer * 0.9f) * 0.1f;
-        pos.z = camRotatorInitPos.z + Mathf.Sin(timer * 1.3f) * 0.02f;
+        pos.y = camRotatorInitPos.y + Mathf.Sin(timer * 1.1f) * 0.1f * multiplier;
+        pos.x = camRotatorInitPos.x + Mathf.Cos(timer * 0.9f) * 0.04f * multiplier;
+        pos.z = camRotatorInitPos.z + Mathf.Sin(timer * 1.3f) * 0.01f * multiplier;
         camObj.transform.position = pos;
     }
 }
