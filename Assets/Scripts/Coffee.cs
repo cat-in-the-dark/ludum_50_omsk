c1ca4f4f642f1a69d2cb3f1c7e8cb772d57d83bd
Ilya Siganov <ilya.blan4@gmail.com>
2022-04-03 00:23:37 +0600

    Fix cup

--- Assets/Scripts/Coffee.cs
+++ Assets/Scripts/Coffee.cs
@@ -1,47 +1,50 @@
 using System;
 using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 
 public class Coffee : MonoBehaviour
 {
+    [SerializeField] private Transform faceAnchor;
     public event Action OnFinishDrinking;
     private bool isDrinking;
-    private Animator animator;
-    
+
     private State state;
 
     private void Start()
     {
         state = State.Find();
-        animator = GetComponent<Animator>();
     }
 
     public void Drink()
     {
-        if (state.energyLevel <= state.maxEnergyLevel)
+        if (state.energyLevel <= state.maxEnergyLevel && !isDrinking)
         {
-            StartCoroutine(StartDrink());
+            StartDrink();
         }
     }
 
-    private IEnumerator StartDrink()
+    private void StartDrink()
     {
-        animator.SetTrigger("Drink");
-        yield return new WaitForFixedUpdate();
         isDrinking = true;
-        StartCoroutine(ApplyEnergy());
         
-        var animState = animator.GetCurrentAnimatorStateInfo(0);
-        if (isDrinking && !animState.IsName("drink"))
-        {
-            isDrinking = false;
-            OnDrink();
-            OnFinishDrinking?.Invoke();
-        }
+        var initPos = transform.position;
+        var initRot = transform.rotation;
+        
+        var seq = DOTween.Sequence();
+        seq.Append(transform.DOMove(faceAnchor.position, 0.5f));
+        seq.Append(transform.DORotate(faceAnchor.rotation.eulerAngles, 0.5f));
+        seq.Append(transform.DOMove(initPos, 0.5f));
+        seq.Join(transform.DORotate(initRot.eulerAngles, 0.25f));
+        seq.OnComplete(OnDrink);
+
+        StartCoroutine(ApplyEnergy());
     }
 
     private void OnDrink()
     {
+        OnFinishDrinking?.Invoke();
+        isDrinking = false;
     }
 
     private IEnumerator ApplyEnergy()
