677d53669dba353fafb864294239b0f4bdc3e87e
Ilya Siganov <ilya.blan4@gmail.com>
2022-04-04 01:45:23 +0600

    fix balance and final screen

--- Assets/Scripts/PressToStart.cs
+++ Assets/Scripts/PressToStart.cs
@@ -1,15 +1,22 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PressToStart : MonoBehaviour
 {
+    private bool canSkip = false;
+    
+    private IEnumerator Start()
+    {
+        canSkip = false;
+        yield return new WaitForSeconds(0.5f);
+        canSkip = true;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKeyDown)
+        if (Input.anyKeyDown && canSkip)
         {
             SceneManager.LoadScene(0);
         }    
